f0aa842d399acb653f3a284045816402afb339b9
scschwartz <scschwa@umich.edu>
2016-07-25 19:47:05 -0400

    Add files via upload

---
+++ changing.js
@@ -0,0 +1,47 @@
+function change_IA(){
+    var display = document.getElementById("IA");
+   
+    display.innerHTML=" ";
+    display.innerHTML = "Instructional Aide for the University of Michigan's Chemical Engineering class, CHE230. I will be holding a 'review' type discussion section, as well as office hours as resources to the students. I will also aide the professor and the rest of the course team to design exams and homework.";
+    display.style.color = 'black';
+}
+
+function change_TA(){
+
+    var display = document.getElementById("TA");
+   
+    display.innerHTML=" ";
+    display.innerHTML = "Teaching assistant for the Girls Who Code summer immersion program. In this program, I helped teach 19 high school girls scratch, python, C++/Arduino, HTML, CSS and JavaScript.";
+    display.style.color = 'black';
+}
+
+function change_research(){
+
+    var display = document.getElementById("research");
+   
+    display.innerHTML=" ";
+    display.innerHTML = "My reasearch consists of timing the compleated percolation of certain lattice patterns using computer simulations.";
+    display.style.color = 'black';
+}
+
+function change_BS(){
+
+    var display = document.getElementById("BS");
+   
+    display.innerHTML=" ";
+    display.innerHTML = "Helped promote the newly launched College Football section of their app, as well as testing the UI."
+    display.style.color = 'black';
+}
+
+function change_able(){
+
+    var display = document.getElementById("able");
+   
+    display.innerHTML=" ";
+    display.innerHTML = "Research and help implement sustainable energy solutions, as well as organize the office place and take notes on meetings.";                                                                       
+
+    display.style.color = 'black';
+}
+
+
+
